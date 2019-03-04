36e32cf8ae4427e646b9f72d8db61e6edcc86a7a
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-03-04 12:20:45 -0500

    add index view and base templates & update .gitignore

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from catalog.models import Book, Author, BookInstance, Genre
 # Create your views here.
+def index(request):
+    """View function for homepage of site."""
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    # 'all()' is implied below
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
