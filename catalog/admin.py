e076736059b4ee8ab11146b5bf827568a5b25306
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-03-04 00:30:55 -0500

    update how data is displayed in the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,54 @@ from django.contrib import admin
 # Register your models here.
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+# Define an admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
+
+# to customize the admin view and functionality, we comment out the registrations above for the classes we want to modify
+
+# this plus 'inlines' in the BookAdmin class allows you to display Book info and BooksInstance info in the same detail view
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Unfortunately we can't directly specify the genre field in `list_display` because it is a `ManyToManyField` (Django prevents this because there would be a large database access "cost" in doing so). Instead we'll define a `display_genre` function to get the information as a string (this is the function we've called above; we'll define it below).
+#
+# Note: Getting the `genre` may not be a good idea here, because of the "cost" of the database operation. We're showing you how because calling functions in your models can be very useful for other reasons — for example to add a Delete link next to every item in the list.
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    # provides panel w filtering options
+    list_filter = ('status', 'due_back')
+    # breaks up form into sections (in this case, one with no label (None) and one titled 'Availablity')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
