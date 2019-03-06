8617f8fda72c8f5e7367a5b9db29bae843192ebf
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-03-05 21:20:53 -0500

    add association btwn user and book instance
    create url view template for user borrowed books

--- catalog/admin.py
+++ catalog/admin.py
@@ -42,7 +42,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back', 'id')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     # provides panel w filtering options
     list_filter = ('status', 'due_back')
     # breaks up form into sections (in this case, one with no label (None) and one titled 'Availablity')
@@ -51,6 +51,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
