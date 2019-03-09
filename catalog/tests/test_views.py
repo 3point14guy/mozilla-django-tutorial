f8591260c1b5bbf73e5be1f017f50cb57862348f
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-03-09 11:21:47 -0500

    add view test for logged in user

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,7 +1,11 @@
+import datetime
+
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-from catalog.models import Author
+from catalog.models import Author, Book, BookInstance, Genre, Language
 
 class AuthorListViewTest(TestCase):
     @classmethod
@@ -40,3 +44,52 @@ class AuthorListViewTest(TestCase):
         self.assertTrue('is_paginated' in response.context)
         self.assertTrue(response.context['is_paginated'] == True)
         self.assertTrue(len(response.context['author_list']) == 3)
+
+class LoanedBooksByUserListViewTest(TestCase):
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password="p@55w0rd")
+        test_user2 = User.objects.create_user(username='testuser2', password="p@55w0rd")
+        test_user1.save()
+        test_user2.save()
+
+        test_author = Author.objects.create(first_name="Joe", last_name="Mama")
+        test_genre = Genre.objects.create(name='Fantasy')
+        test_language = Language.objects.create(name='English')
+        test_book = Book.objects.create(
+            title='Book Title',
+            summary="a little blurb",
+            isbn="123456789",
+            author=test_author,
+            language=test_language,
+        )
+
+        # create genre as a post-step
+        genre_objects_for_book = Genre.objects.all()
+        # direct assignment of many-to-many not allowed
+        test_book.genre.set(genre_objects_for_book)
+        test_book.save()
+
+        number_of_book_copies = 30
+        for book_copy in range(number_of_book_copies):
+            return_date = timezone.now() + datetime.timedelta(days=book_copy%5)
+            the_borrower = test_user1 if book_copy % 2 else test_user2
+            status = 'm'
+            BookInstance.objects.create(
+                book=test_book,
+                imprint='Unlikely Imprint, 2016',
+                due_back = return_date,
+                borrower=the_borrower,
+                status=status,
+            )
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('my-borrowed'))
+        self.assertRedirects(response, '/accounts/login/?next=/catalog/mybooks/')
+
+    def test_logged_in_users_correct_template(self):
+        login = self.client.login(username='testuser1', password="p@55w0rd")
+        response = self.client.get(reverse('my-borrowed'))
+
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/bookinstance_list_borrowed_user.html')
